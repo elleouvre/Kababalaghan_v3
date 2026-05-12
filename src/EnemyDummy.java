3dd73d78e08d684669535b745c12d46fb99d09b4
Sarah Daytia <sdaytia@gmail.com>
2026-05-13 01:06:15 +0800

    fix(Character): correct TestDummy and EnemyDummy errors and implement skill system
    - Fix errors in TestDummy and EnemyDummy character files
    - Implement abstract class with useSkill1, useSkill2, useSkill3 methods
    - Add damage input parameters for respective skills

--- src/EnemyDummy.java
+++ src/EnemyDummy.java
@@ -1,9 +1,21 @@
 public class EnemyDummy extends Character{
-    public EnemyDummy{}{
+    public EnemyDummy(){
         super("Punching bag", 150, 5, 5, 100,
                 "basic",
                 "special skill",
                 "ultimate"
         );
+    }
+    public void useSkill1(Character target){
+        int damage = attack + 5;
+        System.out.println(name + "Uses basic attack");
+    }
+    public void useSkill2(Character target){
+        int damage = attack + 15;
+        System.out.println(name + "Uses special skill");
+    }
+    public void useSkill3(Character target){
+        int damage = attack + 40;
+        System.out.println(name + "Uses ultimate skill");
     }
 }
--- src/TestDummy.java
+++ src/TestDummy.java
@@ -1,9 +1,22 @@
 public class TestDummy extends Character{
-    public TestDummy{}{
-        super("Attacker",100, 5, 5, 100,
+    public TestDummy(){
+        super("Attacker", 100, 5, 5, 100,
                 "basic",
                 "special skill",
                 "ultimate"
         );
+
+    }
+    public void useSkill1(Character target){
+        int damage = attack + 5;
+        System.out.println(name + "Uses basic attack");
+    }
+    public void useSkill2(Character target){
+        int damage = attack + 15;
+        System.out.println(name + "Uses special skill");
+    }
+    public void useSkill3(Character target){
+        int damage = attack + 40;
+        System.out.println(name + "Uses ultimate skill");
     }
 }
