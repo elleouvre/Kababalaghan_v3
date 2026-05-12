b6ad657a1abd16a71104ed70988de5536e0d0cfc
Sarah Daytia <sdaytia@gmail.com>
2026-05-13 00:46:57 +0800

    feat: add TestDummy and EnemyDummy character classes

---
+++ src/EnemyDummy.java
@@ -0,0 +1,9 @@
+public class EnemyDummy extends Character{
+    public EnemyDummy{}{
+        super("Punching bag", 150, 5, 5, 100,
+                "basic",
+                "special skill",
+                "ultimate"
+        );
+    }
+}
---
+++ src/TestDummy.java
@@ -0,0 +1,9 @@
+public class TestDummy extends Character{
+    public TestDummy{}{
+        super("Attacker",100, 5, 5, 100,
+                "basic",
+                "special skill",
+                "ultimate"
+        );
+    }
+}
