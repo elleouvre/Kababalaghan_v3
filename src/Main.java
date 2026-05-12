02b83fefbbaad82f57af94bae6ec714b6fe0ed95
terenceemmanuel <terence.emmanuel@gmail.com>
2026-05-12 23:06:55 +0800

    feat(core): initialize main menu skeleton version

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Skeleton Version of Kababalaghan ");
+        System.out.println("------------------------------");
+        System.out.println("Main Menu");
+        System.out.println();
+        System.out.println("1. Start Game ");
+        System.out.println("2. Quit Game");
+
+    }
+}
