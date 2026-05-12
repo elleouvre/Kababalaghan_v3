f3d5880326082236068347d50b866077523967bf
elleouvre <louellameish.khu@gmail.com>
2026-05-12 23:50:30 +0800

    feat(character): add skill fields, attack attribute, and getters

--- src/Character.java
+++ src/Character.java
@@ -10,13 +10,41 @@ public abstract class Character {
     protected boolean isAlive = true;
     protected Random random = new Random();
 
+    protected String skill1;
+    protected String skill2;
+    protected String skill3;
+
     public Character(String name, String type, int maxHp, int attack, int stamina, int maxStamina,
-                     String basicAttackName, String specialSkillName, String ultimateSkillName) {
+                     String skill1, String skill2, String skill3) {
         this.name = name;
         this.hp = maxHp;
         this.maxHp = maxHp;
         this.stamina = maxStamina;
         this.maxStamina = maxStamina;
         this.attack = attack;
+
+        this.skill1 = skill1;
+        this.skill2 = skill2;
+        this.skill3 = skill3;
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public int getMaxHp() {
+        return maxHp;
+    }
+    public int getStamina() {
+        return stamina;
+    }
+    public int getMaxStamina() {
+        return maxStamina;
+    }
+    public int getAttack() {
+        return attack;
     }
 }
