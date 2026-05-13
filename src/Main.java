000d14bf2fcc746b5f9db5b39134313eaec0d110
Jed Delica <jidelisjnbd@gmail.com>
2026-05-13 21:08:24 +0800

    fix(Main): update main method signature to accept command line arguments

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static void main(){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Skeleton Version of Kababalaghan ");
