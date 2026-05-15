80e2627d25236ab14e6da1fe9f7f52e4ab97a46c
elleouvre <louellameish.khu@gmail.com>
2026-05-15 22:42:14 +0800

    feat(character): add Magwayen hero and Kapre villain classes

---
+++ src/Characters/heroes/Magwayen.java
@@ -0,0 +1,72 @@
+package Characters.heroes;
+
+import Characters.Character;
+
+public class Magwayen extends Character {
+    public Magwayen() {
+        super("Magwayen", "Hero", 140, 15, 120,
+                "Ulap ng Kalaliman",
+                "Luha ng Karagatan",
+                "Pagtawid sa Dagat ng mga Yumao");
+
+        this.basicAttackStaminaCost = 0;
+        this.specialSkillStaminaCost = 20;
+        this.ultimateSkillStaminaCost = 50;
+
+        stamina.setRegenRange(10, 25);
+    }
+
+    @Override
+    public void basicAttack(Character target) {
+
+        if (stamina.spend(basicAttackStaminaCost)) {
+            int damage = attack + random.nextInt(8);
+
+            System.out.println("Umuusbong ang malamlam na ulap mula sa kailaliman...");
+            System.out.println("Naliligaw ang diwa ng kalaban sa hamog nga mga yumao!");
+
+            target.takeDamage(damage);
+            System.out.println(target.getName() + " took " + damage + " damage.");
+            System.out.println(target.getName() + " feels weakened and disoriented.");
+        }
+    }
+
+    @Override
+    public void specialSkill(Character target) {
+
+        if (stamina.spend(specialSkillStaminaCost)) {
+            int damage = attack * 2 + random.nextInt(15);
+
+            System.out.println(name + " uses " + skill2 + "!");
+            System.out.println("Isang matalim na agos ng tubig ang sumasalpok sa kalaban!");
+
+            target.takeDamage(damage);
+            System.out.println(target.getName() + " took " + damage + " damage.");
+
+            int heal = 12;
+            hp = Math.min(maxHp, this.hp + heal);
+            System.out.println(name + " absorbed lost souls and restores " + heal + " HP.");
+        }
+
+    }
+
+    @Override
+    public void ultimateSkill(Character target) {
+
+        if (stamina.spend(ultimateSkillStaminaCost)) {
+            int damage = attack * 3 + random.nextInt(25);
+
+            System.out.println(name + " uses " + skill3 + "!");
+            System.out.println("Nagbubukas ang dagat ng mga yumao... hindi maiiwasan ang pagtawid!");
+
+            target.takeDamage(damage);
+            System.out.println(target.getName() + " took " + damage + " damage.");
+
+            // damages over time (extra hit)
+            int soulDrain = 10;
+            target.takeDamage(soulDrain);
+            System.out.println("The spirits continue to drag " + target.getName() + "...");
+            System.out.println(target.getName() + " suffers " + soulDrain + " soul damage.");
+        }
+    }
+}
---
+++ src/Characters/villains/Kapre.java
@@ -0,0 +1,62 @@
+package Characters.villains;
+
+import Characters.Character;
+
+public class Kapre extends Character {
+    public Kapre() {
+        super("Kapre", "Villain", 170, 17,100,
+                "Suntok ng Higante",
+                "Usok ng Sigarilyo",
+                "Galit ng Bantay-Puno");
+
+        this.basicAttackStaminaCost = 0;
+        this.specialSkillStaminaCost = 18;
+        this.ultimateSkillStaminaCost = 50;
+
+        stamina.setRegenRange(8, 20);
+    }
+
+    @Override
+    public void basicAttack(Character target) {
+        if(stamina.spend(basicAttackStaminaCost)) {
+            int damage = attack + random.nextInt(8);
+
+            System.out.println("Umuuga ang lupa sa lakas ng suntok ng Kapre!");
+
+            target.takeDamage(damage);
+            System.out.println(target.getName() + " took " + damage + " damage.");
+        }
+    }
+
+    @Override
+    public void specialSkill(Character target) {
+
+        if (stamina.spend(specialSkillStaminaCost)) {
+            int damage = attack * 2 + random.nextInt(12);
+
+            System.out.println(name + " uses " + skill2 + "!");
+            System.out.println("Bumabalot ang makapal na usok... naliligaw ang diwa ng kalaban!");
+
+            target.takeDamage(damage);
+            System.out.println(target.getName() + " took " + damage + " damage.");
+
+            int heal = 12;
+            hp = Math.min(maxHp, this.hp + heal);
+            System.out.println(name + " restores " + heal + " HP.");
+        }
+    }
+
+    @Override
+    public void ultimateSkill(Character target) {
+
+        if (stamina.spend(ultimateSkillStaminaCost)) {
+            int damage = attack * 3 + random.nextInt(25) + 10;
+
+            System.out.println(name + " uses " + skill3 + "!");
+            System.out.println("Nagngangalit ang Kapre sa sinumang lumalapastangan sa kanyang punong binabantayan!");
+
+            target.takeDamage(damage);
+            System.out.println(target.getName() + " took " + damage + " damage.");
+        }
+    }
+}
