9cd790db1647b31cc44b874aedefcfe34e415f5c
Sarah Daytia <sdaytia@gmail.com>
2026-05-19 22:14:54 +0800

    fix(character): refactor Mayari and Tikbalang

--- src/z_broken_characters/Mayari.java
+++ src/z_broken_characters/Mayari.java
@@ -1,67 +1,81 @@
-/*
-package z_broken_characters;
+package characters.heroes;
 import characters.Character;
+import java.util.Random;
 
 public class Mayari extends Character {
     private boolean isReadyToCounter = false;
+    private Random random = new Random();
 
     public Mayari() {
         super("Mayari", "Hero", 140, 28, 100,
-                "Piercing Moonlight",
-                "Shroud of the One-Eyed Queen",
-                "Decree of the Celestial Divide");
+                "Tumatagos ang Liwanag ng Buwan",
+                "Saplot ng One-Eyed Queen",
+                "Dekreto ng Celestial Divide");
 
+        this.basicAttackStaminaCost = 5;
         this.specialSkillStaminaCost = 20;
         this.ultimateSkillStaminaCost = 45;
+
+        getStamina().setRegenRange(20, 35);
     }
 
     @Override
     public void basicAttack(Character target) {
-        // Simple 50/50 chance for bonus effects instead of turn tracking
-        int damage = attack + random.nextInt(15);
-        System.out.println("Mayari thrusts her spear! 'The shadows do not hide you.'");
+        if (getStamina().spend(basicAttackStaminaCost)) {
+            int damage = attack + random.nextInt(15);
+            System.out.println(name + " uses " + skill1 + "!");
+            System.out.println("Itinutok ni Mayari ang kanyang sibat! 'Hindi ka itinatago ng mga anino.'");
 
-        if (random.nextBoolean()) {
-            System.out.println("Critical Hit! The Full Moon empowers the strike!");
-            damage += 10;
+            if (random.nextBoolean()) {
+                System.out.println("Kritikal na Hit! Ang Full Moon ay nagbibigay kapangyarihan sa strike!");
+                damage += 10;
+            }
+            target.takeDamage(damage);
         }
-        target.takeDamage(damage);
     }
 
     @Override
     public void specialSkill(Character target) {
-        if (spendStamina(specialSkillStaminaCost)) {
-            System.out.println(name + " enters a Counter Stance! 'Focus on what you can see...'");
-            this.counterReady = true; // Sets the flag for the next hit
+        if (getStamina().spend(specialSkillStaminaCost)) {
+            System.out.println(name + " uses " + skill2 + "!");
+            System.out.println("Pumasok si Mayari sa isang Counter Stance! 'Focus sa kung ano ang makikita mo...'");
+            this.isReadyToCounter = true;
+
+            // Counter stance doesn't deal damage, just prepares for next hit
+            System.out.println("Handa nang kontrahin ang susunod na pag-atake!");
         }
     }
 
     @Override
     public void ultimateSkill(Character target) {
-        if (spendStamina(ultimateSkillStaminaCost)) {
-            int damage = attack * 3 + 20;
-            System.out.println("The sky splits! 'I am the sky's true master!'");
+        if (getStamina().spend(ultimateSkillStaminaCost)) {
+            int damage = attack * 3 + 20 + random.nextInt(15);
+            System.out.println(name + " uses " + skill3 + "!");
+            System.out.println("Nahati ang langit! 'Ako ang tunay na panginoon ng langit!'");
+
             target.takeDamage(damage);
 
-            // Simple shield logic
-            this.hp = Math.min(maxHp, this.hp + 30);
-            System.out.println("A Silver Shield protects Mayari!");
+            // Heal/shield effect
+            int healAmount = 30;
+            this.hp = Math.min(maxHp, this.hp + healAmount);
+            System.out.println("Nag-restore ang isang Silver Shield " + healAmount + " HP to " + name + "!");
         }
     }
 
     @Override
     public void takeDamage(int damage) {
-        // If counter is on, take 0 and heal
-        if (counterReady) {
-            System.out.println("Mayari parries! 0 damage taken and she recovers 20 HP!");
-            this.hp = Math.min(maxHp, this.hp + 20);
-            counterReady = false;
+        // Counter stance logic
+        if (isReadyToCounter) {
+            System.out.println(name + " pinipigilan ang pag-atake! 0 pinsala ang nakuha at nabawi ang 20 HP!");
+            int healAmount = 20;
+            this.hp = Math.min(maxHp, this.hp + healAmount);
+            isReadyToCounter = false;
             return;
         }
 
-        // Permanent 20% reduction to keep it simple instead of toggling
+        // Permanent 20% damage reduction
         int finalDamage = (int)(damage * 0.80);
         super.takeDamage(finalDamage);
+        System.out.println(name + " binabawasan ang pinsala ng 20% dahil sa pagpapala ng buwan!");
     }
 }
-*/
--- src/z_broken_characters/Tikbalang.java
+++ src/z_broken_characters/Tikbalang.java
@@ -1,34 +1,39 @@
-/*
-package z_broken_characters;
+package characters.villains;
 import characters.Character;
+import java.util.Random;
 
 public class Tikbalang extends Character {
+    private Random random = new Random();
 
     public Tikbalang() {
-        super("Tikbalang", "Hero", 120, 30, 100, 100,
+        super("Tikbalang", "Villain", 120, 30, 100,
                 "Hoof Stomp",
                 "Labyrinth of the Forest",
                 "Gingagayuma (Bewitched)");
 
-        // High agility stats - lower health but high damage
+
+        this.basicAttackStaminaCost = 0;
         this.specialSkillStaminaCost = 25;
         this.ultimateSkillStaminaCost = 50;
+        getStamina().setRegenRange(10, 20);
     }
 
     @Override
     public void basicAttack(Character target) {
-        int damage = attack + random.nextInt(15);
-        System.out.println("A powerful kick from iron hooves!");
-        target.takeDamage(damage);
+        if (getStamina().spend(basicAttackStaminaCost)) {
+            int damage = attack + random.nextInt(15);
+            System.out.println(name + " uses " + skill1 + "!");
+            System.out.println("Isang malakas na sipa mula sa mga bakal!");
+            target.takeDamage(damage);
+        }
     }
 
     @Override
     public void specialSkill(Character target) {
-        if (spendStamina(specialSkillStaminaCost)) {
-            // Confusion/Trickster logic: Small damage + high miss chance
-            int damage = attack + 10;
-            System.out.println(name + " uses " + specialSkillName + "!");
-            System.out.println("The forest paths begin to loop. The enemy is disoriented!");
+        if (getStamina().spend(specialSkillStaminaCost)) {
+            int damage = attack + 10 + random.nextInt(10);
+            System.out.println(name + " uses " + skill2 + "!");
+            System.out.println("Nagsisimulang umikot ang mga landas sa kagubatan. Ang kalaban ay disoriented!");
             target.takeDamage(damage);
 
             // In a real battle manager, you'd add a "Confused" status here
@@ -38,16 +43,15 @@ public class Tikbalang extends Character {
 
     @Override
     public void ultimateSkill(Character target) {
-        if (spendStamina(ultimateSkillStaminaCost)) {
-            // Massive damage based on the myth of the Tikbalang's immense strength
+        if (getStamina().spend(ultimateSkillStaminaCost)) {
             int damage = (attack * 4) + random.nextInt(20);
-            System.out.println(name + " uses " + ultimateSkillName + "!");
-            System.out.println("A thick smoke of tobacco fills the air! The Tikbalang strikes from the shadows!");
+            System.out.println(name + " uses " + skill3 + "!");
+            System.out.println("Isang makapal na usok ng tabako ang pumupuno sa hangin! Ang Tikbalang ay tumatama mula sa mga anino!");
             target.takeDamage(damage);
 
             // Speed buff: Restore some stamina to represent supernatural speed
-            this.stamina = Math.min(maxStamina, this.stamina + 20);
-            System.out.println(name + " moves with supernatural speed, regaining some stamina!");
+            getStamina().add(20);  //
+            System.out.println(name + " gumagalaw nang may supernatural na bilis, na nagbabalik ng 20 stamina!");
         }
     }
 
@@ -56,10 +60,9 @@ public class Tikbalang extends Character {
         // Passive: "Trickster's Haze"
         // 20% chance to take 0 damage by "vanishing" into the forest
         if (random.nextInt(100) < 20) {
-            System.out.println(name + " vanished into the trees! The attack missed!");
+            System.out.println(name + " nawala sa mga puno! Hindi nakuha ang pag-atake!");
             return;
         }
         super.takeDamage(damage);
     }
 }
-*/
