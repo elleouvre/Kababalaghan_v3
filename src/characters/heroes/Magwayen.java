26cfdad13d00fbf86d21f11b68eb401c57e3f4fe
elleouvre <louellameish.khu@gmail.com>
2026-05-19 23:43:27 +0800

    feat(characters): fixed accuracy stats to Magwayen and Kapre

--- src/characters/heroes/Magwayen.java
+++ src/characters/heroes/Magwayen.java
@@ -5,7 +5,7 @@ import characters.Character;
 public class Magwayen extends Character {
     public Magwayen() {
         super("Magwayen", "Hero", 140, 15, 120,
-                0.95,0.80,0.65,
+                0.80,0.60,0.30,
                 "Ulap ng Kalaliman",
                 "Luha ng Karagatan",
                 "Pagtawid sa Dagat ng mga Yumao");
