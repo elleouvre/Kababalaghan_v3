518ecdafc99a2a45d8f4a6ab2dbcc30bed13c286
elleouvre <louellameish.khu@gmail.com>
2026-05-21 23:28:57 +0800

    feat(util): add Colors class with ANSI color constants

---
+++ src/util/Colors.java
@@ -0,0 +1,25 @@
+package util;
+
+public class Colors {
+    //resets the color
+    public static final String RESET  = "\u001B[0m";
+
+    //general colors
+    public static final String BLACK   = "\u001B[30m";
+    public static final String RED     = "\u001B[31m";
+    public static final String GREEN   = "\u001B[32m";
+    public static final String YELLOW  = "\u001B[33m";
+    public static final String BLUE    = "\u001B[34m";
+    public static final String PURPLE  = "\u001B[35m";
+    public static final String CYAN    = "\u001B[96m";
+    public static final String WHITE   = "\u001B[37m";
+    public static final String LIGHT_BLUE    = "\u001B[96m";
+
+    //border
+    public static final String SILVER       = "\u001B[38;5;102m";
+
+    //moon (for game intro display)
+    public static final String MOON_WHITE  = "\u001B[38;5;255m";
+    public static final String MOON_SILVER = "\u001B[38;5;188m";
+    public static final String MOON_GREY   = "\u001B[38;5;102m";
+}
