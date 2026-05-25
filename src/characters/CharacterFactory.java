cefae41ffeba23813843b37a667b7a9d4eef75f6
terenceemmanuel <terence.emmanuel@gmail.com>
2026-05-26 00:24:08 +0800

    refactor(character-factory): improve enemy generation logic and clean up structure

--- src/characters/CharacterFactory.java
+++ src/characters/CharacterFactory.java
@@ -42,42 +42,42 @@ public class CharacterFactory {
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println(Colors.PURPLE + "                                       ╔═══════════════════════════════════════════════════╗" + Colors.RESET);
-        System.out.println(Colors.MOON_WHITE+ "                                       ║                   CHARACTERS:                     ║" + Colors.RESET);
-        System.out.println(Colors.PURPLE + "                                       ╚═══════════════════════════════════════════════════╝" + Colors.RESET);
+        System.out.println(Colors.MOON_GREY + "                                           ╔════════════════════════════════════════════╗" + Colors.RESET);
+        System.out.println(Colors.MOON_GREY+ "                                           ║"+Colors.MOON_WHITE+Colors.ITALIC+Colors.BOLD+"                 CHARACTERS:                "+Colors.MOON_GREY+"║" + Colors.RESET);
+        System.out.println(Colors.MOON_GREY + "                                           ╚════════════════════════════════════════════╝" + Colors.RESET);
 
-            System.out.println(Colors.BLUE + "                \n╠  HEROES  ╣" + Colors.RESET);
-            System.out.println(Colors.BLUE + "════════════════════════════════════════════════════════════" + Colors.RESET);
+            System.out.println(Colors.BLUE + "                                                           \n                                           ╠  HEROES  ╣" + Colors.RESET);
+            System.out.println(Colors.BLUE + "                                           ════════════════════════════════════════════════════════════" + Colors.RESET);
             ArrayList<Character> heroes = getAllHeroes();
             for (int i = 0; i < heroes.size(); i++) {
                 Character hero = heroes.get(i);
-                System.out.printf("%d. %-15s HP: %3d | ATK: %3d | STA: %3d%n",
+                System.out.printf(Colors.ITALIC+Colors.MOON_WHITE+"                                           %d. %-15s HP: %3d | ATK: %3d | STA: %3d%n",
                         (i+1), hero.getName(), hero.getMaxHp(), hero.getAttack(), hero.getStaminaMax());
-                System.out.printf("   Skills: %s, %s, %s%n",
+                System.out.printf(Colors.MOON_GREY+"                                              Skills: %s, %s, %s%n",
                         hero.getBasic(),
                         hero.getSpecial(),
                         hero.getUltimate());
-                System.out.printf("   Stamina Regen: %d-%d per round%n",
+                System.out.printf(Colors.BLUE+"                                              Stamina Regen: %d-%d per round%n",
                         hero.getStaminaRegenMin(),
                         hero.getStaminaRegenMax());
             }
 
-        System.out.println(Colors.RED + "                \n╠  VILLAINS  ╣" + Colors.RESET);
-        System.out.println(Colors.RED + "════════════════════════════════════════════════════════════" + Colors.RESET);
+        System.out.println(Colors.RED + "                                                           \n                                           ╠  VILLAINS  ╣" + Colors.RESET);
+        System.out.println(Colors.RED + "                                           ════════════════════════════════════════════════════════════" + Colors.RESET);
             ArrayList<Character> villains = getAllVillains();
             for (int i = 0; i < villains.size(); i++) {
                 Character villain = villains.get(i);
-                System.out.printf("%d. %-15s HP: %3d | ATK: %3d | STA: %3d%n",
+                System.out.printf(Colors.ITALIC+Colors.MOON_WHITE+"                                           %d. %-15s HP: %3d | ATK: %3d | STA: %3d%n",
                         (i+1), villain.getName(), villain.getMaxHp(), villain.getAttack(), villain.getStaminaMax());
-                System.out.printf("   Skills: %s, %s, %s%n",
+                System.out.printf(Colors.MOON_GREY+"                                              Skills: %s, %s, %s%n",
                         villain.getBasic(),
                         villain.getSpecial(),
                         villain.getUltimate());
-                System.out.printf("   Stamina Regen: %d-%d per round%n",
+                System.out.printf(Colors.BLUE+"                                              Stamina Regen: %d-%d per round%n",
                         villain.getStaminaRegenMin(),
                         villain.getStaminaRegenMax());
             }
-        System.out.println(Colors.PURPLE + "════════════════════════════════════════════════════════════" + Colors.RESET);
+        System.out.println(Colors.PURPLE + "                                           ════════════════════════════════════════════════════════════" + Colors.RESET);
     }
 
     public static Character generateRandomEnemy(boolean isHero) {
@@ -85,15 +85,15 @@ public class CharacterFactory {
 
         if(isHero) {
             enemies = CharacterFactory.getAllVillains();
-            System.out.println("\n[As a Hero, you will fight a Corrupted Villain!]");
+            System.out.println("\n                                           [As a Hero, you will fight a Corrupted Villain!]");
         } else {
             enemies = CharacterFactory.getAllHeroes();
-            System.out.println("\n[As a Villain, you will fight a Corrupted Hero!]");
+            System.out.println("\n                                           [As a Villain, you will fight a Corrupted Hero!]");
         }
 
         Random rand = new Random();
         Character enemy = enemies.get(rand.nextInt(enemies.size()));
-        System.out.println("[Your enemy is: " + enemy.getName() + "]");
+        System.out.println("                                           [Your enemy is: " + enemy.getName() + "]");
 
         return enemy;
     }
